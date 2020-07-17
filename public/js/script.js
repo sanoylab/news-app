c113fab2d04cafbeaefe2f6011ce7632bab48bd6
sanoylab <expertsanoy@gmail.com>
2020-07-17 13:34:33 -0400

    adding default image icon

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,7 @@ const news = async ()=>{
         element +=`
         <div class="col-md-4">
             <div class="card mb-4 shadow-sm">
-           <img style="width: 100%;" src="${news.urlToImage}">
+           <img style="width: 100%;" src="${news.urlToImage ? news.urlToImage : 'https://cbsnews3.cbsistatic.com/hub/i/r/2020/07/17/54956198-96c8-45f3-a2a9-ceec35d9c247/thumbnail/1200x630/6fefdeab206aaec0fe1e11410f77d682/gettyimages-1193343235.jpg'}">
             <div class="card-body">
                 <p class="card-text"><a href="${news.url}" target="_blank">${news.title}</a></p>
                 <div class="d-flex justify-content-between align-items-center">
